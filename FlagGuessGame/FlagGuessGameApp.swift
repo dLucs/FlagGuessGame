e646dc4e95070b830936ee1c664aebb453ec3f33
dLucs <lucas_61@hotmail.es>
2023-12-26 12:25:54 +0100

    Initial Commit

---
+++ FlagGuessGame/FlagGuessGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlagGuessGameApp.swift
+//  FlagGuessGame
+//
+//  Created by Lucas on 26/12/23.
+//
+
+import SwiftUI
+
+@main
+struct FlagGuessGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
